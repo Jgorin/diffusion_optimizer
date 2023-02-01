db8085c20bfb2528b94383dbcee40dcec2baa077
dgorin1 <91497821+dgorin1@users.noreply.github.com>
2023-01-31 20:13:57 -0800

    added pyswarm to juypter notebook

--- main/pipeline.sh
+++ main/pipeline.sh
@@ -5,18 +5,10 @@ FILE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $FILE_DIR/../diffusion_optimizer/src/diffusion_optimizer
 
-if $PROFILE != "false"
-then
 python3.10 interface.py \
     --output_path "$FILE_DIR/output/default_output" \
     --config_path "$FILE_DIR/config/default_config.yaml" \
-    -parameter_tuning \
-    -generate_inputs
-else
-echo "running in cproile mode"
-python3.10 -m cProfile -o performance.pstats interface.py \
-    --output_path "$FILE_DIR/output/default_output" \
-    --config_path "$FILE_DIR/config/default_config.yaml" \
-    -parameter_tuning \
-    -generate_inputs
-fi
+    -parameter_tuning
+    # -generate_inputs
+
+
